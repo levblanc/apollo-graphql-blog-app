87d70939ba15043b9fbe49fc5066a55925ee0715
levblanc <levblanc@gmail.com>
2023-01-28 11:17:19 +0800

    Add user signup mutation & resolver

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,4 +1,6 @@
-import { objectType } from 'nexus';
+import { mutationField, nonNull, objectType, stringArg } from 'nexus';
+import validator from 'validator';
+import { createResponse } from '../utils';
 import { Post } from './Post';
 import { Profile } from './Profile';
 
@@ -21,5 +23,66 @@ export const User = objectType({
           .posts();
       },
     });
+  },
+});
+
+export const UserResponse = objectType({
+  name: 'UserResponse',
+  definition(t) {
+    t.implements('Response');
+    t.field('user', { type: User });
+  },
+});
+
+export const signup = mutationField('signup', {
+  type: UserResponse,
+  args: {
+    email: nonNull(stringArg()),
+    name: stringArg(),
+    password: nonNull(stringArg()),
+  },
+  async resolve(_parent, { email, name, password }, { prisma }) {
+    try {
+      const isEmail = validator.isEmail(email);
+
+      if (!isEmail) {
+        return createResponse({
+          success: false,
+          error: new Error('Invalid email'),
+        });
+      }
+
+      const isValidPassword = validator.isLength(password, { min: 5 });
+
+      if (!isValidPassword) {
+        return createResponse({
+          success: false,
+          error: new Error('Invalid password, length less than 5'),
+        });
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+          password,
+        },
+      });
+
+      const res = createResponse({
+        success: true,
+        message: 'User create success',
+        data: { user },
+      });
+
+      return res;
+    } catch (error) {
+      const err = createResponse({
+        success: false,
+        error,
+      });
+
+      return err;
+    }
   },
 });
