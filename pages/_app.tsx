2b824cdbc74ebc42838df7a8c636375b9e4fc4eb
levblanc <levblanc@gmail.com>
2023-02-06 10:44:37 +0800

    Add Mantine packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
-import '@/styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '@/lib/apolloClient';
+import { MantineProvider } from '@mantine/core';
+import '@/styles/global.css';
+import AppHeader from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Blog App</title>
+        <meta
+          name="description"
+          content="Blog app built with React, Apollo Server,  GraphQL, and PostgreSQL "
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ApolloProvider client={apolloClient}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: 'dark',
+          }}
+        >
+          <div className="appContainer">
+            <AppHeader />
+            <Component {...pageProps} />
+            {/* <Footer /> */}
+          </div>
+        </MantineProvider>
+      </ApolloProvider>
+    </>
   );
 }
