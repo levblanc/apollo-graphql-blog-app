2dcdb1178da4624e81bc07bab58a2eb129bc9fbd
levblanc <levblanc@gmail.com>
2023-02-07 18:04:08 +0800

    Add `profile` UI

---
+++ pages/profile/index.tsx
@@ -0,0 +1,42 @@
+import { Box, Title } from '@mantine/core';
+
+export default function Profile() {
+  const profile = {
+    user: { name: '', posts: [] },
+    bio: '',
+    isMyProfile: false,
+  };
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          marginBottom: '2rem',
+          display: 'flex ',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box>
+          <Title order={2}>{profile.user.name}</Title>
+          <p>{profile.bio}</p>
+        </Box>
+        <div>{profile.isMyProfile ? <AddPostModal /> : null}</div>
+      </Box>
+      {/* <div>
+        {profile.user.posts.map((post) => {
+          return (
+            <Post
+              title={post.title}
+              content={post.content}
+              date={post.createdAt}
+              user={profile.user.name}
+              published={post.published}
+              isMyProfile={profile.isMyProfile}
+              id={post.id}
+            />
+          );
+        })}
+      </div> */}
+    </Box>
+  );
+}
