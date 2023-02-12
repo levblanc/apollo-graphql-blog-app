18f1b185e3f623a1949a3975117c30e80e260d4a
levblanc <levblanc@gmail.com>
2023-02-12 12:39:38 +0800

    Extract `Error` component

---
+++ components/Error/index.tsx
@@ -0,0 +1,14 @@
+import { Paper, Text, Title } from '@mantine/core';
+
+export default function Error({ message }: { message: string }) {
+  return (
+    <Paper withBorder shadow="md" p="md" radius="md">
+      <Title order={4} color="red">
+        ERROR:
+      </Title>
+      <Text fz="lg" c="red">
+        {message}
+      </Text>
+    </Paper>
+  );
+}
