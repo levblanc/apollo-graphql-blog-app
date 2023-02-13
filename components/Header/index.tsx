bc6049023023457bd13b2fee4f1615a2439ce521
levblanc <levblanc@gmail.com>
2023-02-13 15:44:38 +0800

    Update display logic of `Sign Up` & `Log In` buttons in app header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,31 +9,41 @@ import {
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import useStyles from './styles';
+import { TOKEN } from '@/utils/constants';
+import { useEffect, useState } from 'react';
 
 export default function AppHeader() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
   const { classes } = useStyles();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setIsAuthenticated(!!localStorage.getItem(TOKEN));
+  }, []);
 
   return (
     <Box pb={120}>
-      <Header height={60} px="md">
-        <Group position="apart" sx={{ height: '100%' }}>
+      <Header height={80} p="md">
+        <Group position="apart">
           <Title order={1}>Blog App</Title>
-          <Group className={classes.hiddenMobile}>
-            <Button component="a" variant="default" href="/login">
-              Log in
-            </Button>
-            <Button component="a" href="/sign-up">
-              Sign up
-            </Button>
-          </Group>
-
-          <Burger
-            opened={drawerOpened}
-            onClick={toggleDrawer}
-            className={classes.hiddenDesktop}
-          />
+          {!isAuthenticated && (
+            <>
+              <Group className={classes.hiddenMobile}>
+                <Button component="a" variant="default" href="/login">
+                  Log in
+                </Button>
+                <Button component="a" href="/sign-up">
+                  Sign up
+                </Button>
+              </Group>
+              <Burger
+                opened={drawerOpened}
+                onClick={toggleDrawer}
+                className={classes.hiddenDesktop}
+              />
+            </>
+          )}
         </Group>
       </Header>
 
