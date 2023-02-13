7bc846bacba24868003e3fbef70403fa156ae7df
levblanc <levblanc@gmail.com>
2023-02-13 14:28:57 +0800

    Add margin bottom in `Error` component

--- components/Error/index.tsx
+++ components/Error/index.tsx
@@ -2,7 +2,7 @@ import { Paper, Text, Title } from '@mantine/core';
 
 export default function Error({ message }: { message: string }) {
   return (
-    <Paper withBorder shadow="md" p="md" radius="md">
+    <Paper withBorder shadow="md" p="md" radius="md" mb="md">
       <Title order={4} color="red">
         ERROR:
       </Title>
