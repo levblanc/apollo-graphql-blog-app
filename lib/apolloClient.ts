cc1eb5555c2a2ff40d8fc04b708ecff8eec0c001
levblanc <levblanc@gmail.com>
2023-02-13 11:06:21 +0800

    Set saved token (from browser local storage) into Apollo Client context

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/api/graphql',
+});
+
+const authLink = setContext((_request, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
