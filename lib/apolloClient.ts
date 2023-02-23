00687340ff6e101e73429dafdf6b45b3cf85a961
levblanc <levblanc@gmail.com>
2023-02-23 17:29:52 +0800

    Update graphql request domain based on env variable

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -2,8 +2,17 @@ import { TOKEN } from '@/utils/constants';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+type UriMappingType = {
+  [key: string]: string;
+};
+
+const uriMapping: UriMappingType = {
+  development: 'http://localhost:3000',
+  production: 'https://apollo-graphql-blogify.vercel.app',
+};
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: `${uriMapping[process.env.NEXT_PUBLIC_ENV as string]}/api/graphql`,
 });
 
 const authLink = setContext((_request, { headers }) => {
